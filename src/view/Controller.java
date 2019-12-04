079a6f8bc6a3a3bc427c3331f99b364900da2090
B <293183@via.dk>
2019-12-04 12:52:12 +0100

    V0.02 Changes to interface and Controller. Also implemented method body for the Controller

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,65 +1,102 @@
 package view;
 
-import model.Exam;
-import model.ExamCalendarController;
-import model.Person;
-import model.Room;
+import model.*;
 
 import java.util.ArrayList;
 
 public class Controller implements ExamCalendarController
 {
+  // Start of field variables
+  private ArrayList<Person> persons;
+  private ArrayList<Exam> exams;
+  private ArrayList<Room> rooms;
+  // End of field variables
+
+  // CONSTRUCTOR
+  public Controller()
+  {
+    persons = new ArrayList<>();
+    exams = new ArrayList<>();
+    rooms = new ArrayList<>();
+  }
+
   // Start of all getters
-  public Object getPrivateCalendar(Object obj)
+  public PrivateCalendar getPrivateCalendar(Object obj)
   {
-
+    if (obj instanceof Person)
+    {
+      return ((Person) obj).getPrivateCalendar();
+    }
+    else if (obj instanceof Exam)
+    {
+      return ((Exam) obj).getPrivateCalendar();
+    }
+    else if (obj instanceof Room)
+    {
+      return ((Room) obj).getPrivateCalendar();
+    }
+    return null;
   };
-  public ArrayList<Person> getAllPersons(Exam exam)
+  public ArrayList<Person> getAllPersonsAssignedExam(Exam exam)
   {
-
+    ArrayList<Person> allPersons = exam.getAllStudents();
+    for (int i = 0; i < exam.getTeacher().size(); i++)
+    {
+      allPersons.add(exam.getTeacher().get(i));
+    }
+    return allPersons;
   };
-  public Room getRoom(Exam exam)
-  {
 
+  public ArrayList<Person> getAllPersons()
+  {
+    return persons;
+  }
+  public ArrayList<Room> getRooms()
+  {
+    return rooms;
   };
-  public Exam getExam(Room room)
+  public ArrayList<Exam> getExams()
   {
-
+    return exams;
   };
   // End of all getters
 
   // Start of all setters
-  public Person addPerson(String name, int VIAID, ArrayList<String> assignedCourses, boolean isTeacher)
+  public void addPerson(String name, int VIAID, ArrayList<String> assignedCourses, boolean isTeacher)
   {
-
+    Person person = new Person(name, VIAID, assignedCourses, isTeacher);
+    persons.add(person);
   };
-  public Exam addExam(String courseName, double duration, Room priorityRoom, boolean isGroupExam, boolean isWrittenExam)
+  public void addExam(String courseName, double duration, Room priorityRoom, boolean isGroupExam, boolean isWrittenExam)
   {
-
+    Exam exam = new Exam(courseName, duration, priorityRoom, isGroupExam, isWrittenExam);
+    exams.add(exam);
   };
-  public Exam addExam(String courseName, double duration, boolean isGroupExam, boolean isWrittenExam)
+  public void addExam(String courseName, double duration, boolean isGroupExam, boolean isWrittenExam)
   {
-
+    Exam exam = new Exam(courseName, duration, isGroupExam,isWrittenExam);
+    exams.add(exam);
   };
-  public Room addRoom(int studentCapacity, boolean hasHDMI, boolean hasVGA, boolean hasProjector, String roomName)
+  public void addRoom(int studentCapacity, boolean hasHDMI, boolean hasVGA, boolean hasProjector, String roomName)
   {
-
+    Room room = new Room(studentCapacity, hasHDMI, hasVGA, hasProjector, roomName);
+    rooms.add(room);
   };
   // End of all setters
 
 
   // Start of logic
-  public Person removePerson(Person person)
+  public void removePerson(Person person)
   {
-
+    persons.remove(person);
   };
-  public Exam removeExam(Exam exam)
+  public void removeExam(Exam exam)
   {
-
+    exams.remove(exam);
   };
-  public Room removeRoom(Room room)
+  public void removeRoom(Room room)
   {
-
+    rooms.remove(room);
   };
   // End of logic
 }
