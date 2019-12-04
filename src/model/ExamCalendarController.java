8c10cece22d92d7cdf8b05a303c3c0145beb9988
B <293183@via.dk>
2019-12-04 10:31:39 +0100

    Created ExamCalendarController interface

--- src/model/ExamCalendarController.java
+++ src/model/ExamCalendarController.java
@@ -1,15 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+
 public interface ExamCalendarController
 {
-  public Room getRoom();
-  public Exam getExam();
-  public Person getPerson();
-  public Date getDate();
-  public PrivateCalendar getPrivateCalendar();
-  public ExamCalendar getExamCalendar();
-  public boolean roomHasHDMI(Room room);
-  public boolean roomHasVGA(Room room);
-  public boolean roomHasProjector(Room room);
-  
+  // Start of all getters
+  public Object getPrivateCalendar(Object obj);
+  public ArrayList<Person> getAllPersons(Exam exam);
+  public Room getRoom(Exam exam);
+  public Exam getExam(Room room);
+  // End of all getters
+
+  // Start of all setters
+  public Person addPerson(String name, int VIAID, ArrayList<String> assignedCourses, boolean isTeacher);
+  public Exam addExam(String courseName, double duration, Room priorityRoom, boolean isGroupExam, boolean isWrittenExam);
+  public Exam addExam(String courseName, double duration, boolean isGroupExam, boolean isWrittenExam);
+  public Room addRoom(int studentCapacity, boolean hasHDMI, boolean hasVGA, boolean hasProjector, String roomName);
+  // End of all setters
+
+
+  // Start of logic
+  public Person removePerson(Person person);
+  public Exam removeExam(Exam exam);
+  public Room removeRoom(Room room);
+  // End of logic
 }
