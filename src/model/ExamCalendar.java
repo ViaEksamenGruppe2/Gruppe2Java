3f266fa41614218dd33a80f0952f88ce5844922f
B <293183@via.dk>
2019-12-05 15:27:09 +0100

    Ma

--- src/model/ExamCalendar.java
+++ src/model/ExamCalendar.java
@@ -1,5 +1,26 @@
 package model;
 
-public class ExamCalendar extends PrivateCalendar
+import java.util.ArrayList;
+
+public class ExamCalendar
 {
+  private Date firstExamDate;
+  private Date lastExamDate;
+  private ArrayList<Person> persons;
+  private ArrayList<Room> rooms;
+  private ArrayList<Exam> exams;
+
+  public ExamCalendar(Date firstExamDate, Date lastExamDate, ArrayList<Person> persons, ArrayList<Room> rooms, ArrayList<Exam> exams)
+  {
+    this.firstExamDate = firstExamDate;
+    this.lastExamDate = lastExamDate;
+    this.persons = persons;
+    this.rooms = rooms;
+    this.exams = exams;
+  }
+
+  public void generateExamSchedule()
+  {
+    
+  }
 }
