5f7f0d381bf82d38d17792c673461fb7c0e2429f
B <293183@via.dk>
2019-12-09 10:34:24 +0100

    EXAMCALENDAR LOGICS

--- src/model/ExamCalendar.java
+++ src/model/ExamCalendar.java
@@ -19,8 +19,56 @@ public class ExamCalendar
     this.exams = exams;
   }
 
-  public void generateExamSchedule()
+  public ArrayList<ArrayList<Object>> generateExamSchedule()
   {
-
+    // STRUCTURE FOR 2D ARRAYLIST:
+    // DATE
+    // ROOM
+    // EXAM
+    // TEACHERS
+    ArrayList<ArrayList<Object>> examPlanList = new ArrayList<>();
+    Date plannerDate;
+    for (int i = 0; i < exams.size(); i++)
+    {
+      plannerDate = firstExamDate.copy();
+      while (exams.get(i).getPrivateCalendar().isBooked(plannerDate) || exams.get(i).getTeacher().get(0).getPrivateCalendar().isBooked(plannerDate))
+      {
+        plannerDate.stepForwardOneDay();
+        plannerDate = plannerDate.copy();
+      }
+        if (exams.get(i).getPriorityRoom() != null && !exams.get(i).getPriorityRoom().getPrivateCalendar().isBooked(plannerDate) && !exams.get(i).getTeacher().get(0).getPrivateCalendar().isBooked(plannerDate))
+        {
+          exams.get(i).getPrivateCalendar().makeReservation(plannerDate);
+          exams.get(i).getPriorityRoom().getPrivateCalendar().makeReservation(plannerDate);
+          exams.get(i).getTeacher().get(0).getPrivateCalendar().makeReservation(plannerDate);
+          // NEEDS AN AUTO UPDATE ON CALENDAR FOR TEACHER AND STUDENT. AN UPDATE ALL CALENDAR FUNCTION BASED ON DATE
+          ArrayList<Object> addList = new ArrayList<>();
+          addList.add(plannerDate);
+          addList.add(exams.get(i).getPriorityRoom());
+          addList.add(exams.get(i));
+          addList.add(exams.get(i).getTeacher());
+          examPlanList.add(addList);
+        }
+        else if (!exams.get(i).getPrivateCalendar().isBooked(plannerDate) && !exams.get(i).getTeacher().get(0).getPrivateCalendar().isBooked(plannerDate))
+        {
+          for (int j = 0; j < rooms.size(); j++)
+          {
+            if (rooms.get(j).isAvailable(plannerDate))
+            {
+              exams.get(i).getPrivateCalendar().makeReservation(plannerDate);
+              rooms.get(j).getPrivateCalendar().makeReservation(plannerDate);
+              exams.get(i).getTeacher().get(0).getPrivateCalendar().makeReservation(plannerDate);
+              ArrayList<Object> addList = new ArrayList<>();
+              addList.add(plannerDate);
+              addList.add(rooms.get(j));
+              addList.add(exams.get(i));
+              addList.add(exams.get(i).getTeacher());
+              examPlanList.add(addList);
+              break;
+            }
+          }
+        }
+    }
+    return examPlanList;
   }
 }
